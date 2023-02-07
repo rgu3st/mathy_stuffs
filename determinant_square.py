a140af9333f3343cbe8fda2917fca1eb0ed2bf4f
rgu3st <68740638+rgu3st@users.noreply.github.com>
2023-02-07 20:16:25 +0000

    Added basic determinant solution / tests

---
+++ determinant_square.py
@@ -0,0 +1,44 @@
+'''Runs a couple tests to recursively find the determinant of a sqaure matrix.'''
+
+def get_minor(matrix, col_pos):
+    '''Returns a duplicate matrix, less the first row and ith column.'''
+    ret = []
+    n = len(matrix)
+    for i in range(1, n):
+        ret_row = []
+        for j in range(0, n):
+            if j == col_pos:
+                continue
+            ret_row.append(matrix[i][j])
+        ret.append(ret_row)
+    return ret
+
+
+def determinant(matrix):
+    '''Recursivley solves determinants to a given square matrix. No safety/sanity checks for now...'''
+    if matrix is None:
+        return()
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+
+    det = 0
+    
+    for i in range(0, n):
+        # Recurse on the subs:
+        minor = get_minor(matrix, i)
+        if i % 2 == 0:
+            det += matrix[0][i] * determinant(minor)
+        else:
+            det -= matrix[0][i] * determinant(minor)
+    return det
+
+
+if __name__ == "__main__":
+    test1 = [[1,2,3], [4,5,6], [7,8,9]]
+    result1 = determinant(test1)
+    assert result1 == 0
+
+    test2 = [[4,6],[3,8]]
+    result2 = determinant(test2)
+    assert result2 == 14
